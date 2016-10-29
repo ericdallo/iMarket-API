9843438d1241550089b318ba12672a7a2689e2dc
Eric Dallo <ericdallo06@hotmail.com>
2016-10-28 22:19:42 -0200

    Adciiona listagem de premarkets

--- src/main/java/br/com/imarket/premarket/PreMarketController.java
+++ src/main/java/br/com/imarket/premarket/PreMarketController.java
@@ -1,8 +1,11 @@
 package br.com.imarket.premarket;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.composed.web.rest.json.GetJson;
 import org.springframework.composed.web.rest.json.PostJson;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,10 @@ public class PreMarketController {
 	@PostJson("/premarkets")
 	public void create(@Valid @RequestBody PreMarket preMarket) {
 		preMarketRepository.save(preMarket);
+	}
+	
+	@GetJson("/premarkets")
+	public List<PreMarket> list() {
+		return preMarketRepository.findAll();
 	}
 }
--- src/main/java/br/com/imarket/premarket/PreMarketRepository.java
+++ src/main/java/br/com/imarket/premarket/PreMarketRepository.java
@@ -1,10 +1,14 @@
 package br.com.imarket.premarket;
 
+import java.util.List;
+
 import org.springframework.data.repository.RepositoryDefinition;
 
 @RepositoryDefinition(domainClass = PreMarket.class, idClass = Long.class)
 public interface PreMarketRepository {
 
 	void save(PreMarket preMarket);
+
+	List<PreMarket> findAll();
 	
 }
