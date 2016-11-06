aa5dcd9b266c55b352c4014e8be668e29034b267
Eric Dallo <ericdallo06@hotmail.com>
2016-11-06 12:11:49 -0200

    Corrige deploy

--- docker/startup.sh
+++ docker/startup.sh
@@ -7,4 +7,4 @@ cd $IMARKET_API_HOME
 
 SPRING_CONFIG="--spring.config.location=file://$IMARKET_API_PROPERTIES"
 
-exec java -jar build/libs/iMarket-API.jar $SPRING_CONFIG
+exec java -jar build/libs/app.jar $SPRING_CONFIG
