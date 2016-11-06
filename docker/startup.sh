e90b61161c0a54756f7c46f2c61aad3787e4e7b5
Eric Dallo <ericdallo06@hotmail.com>
2016-11-06 11:21:33 -0200

    Corrige deploy

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -ve
 
-BUCKET_DIR=/tmp/bucket
 IMARKET_API_PROPERTIES=/opt/application.properties
-
-mkdir -p $BUCKET_DIR
-gcsfuse configuration.imarketbr.com $BUCKET_DIR
-
-cp $BUCKET_DIR/imarket-api/prod/production.properties $IMARKET_API_PROPERTIES
-fusermount -u $BUCKET_DIR
-rm -r $BUCKET_DIR
 
 cd $IMARKET_API_HOME
 
--- scripts/deploy-in-instance.sh
+++ scripts/deploy-in-instance.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+BUCKET_DIR=/tmp/bucket
+IMARKET_API_PROPERTIES=/opt/application.properties
+
+mkdir -p $BUCKET_DIR
+gcsfuse configuration.imarketbr.com $BUCKET_DIR
+
+cp $BUCKET_DIR/imarket-api/prod/production.properties $IMARKET_API_PROPERTIES
+fusermount -u $BUCKET_DIR
+rm -r $BUCKET_DIR
+
 docker pull imarket/imarket-api:latest
 docker stop imarket-api
 docker rm imarket-api
-docker run --name imarket-api -d -p 80:9090 -v /tmp/production.properties:/opt/app/app/src/js/env.js imarket/imarket-web
+docker run --name imarket-api -d -v $IMARKET_API_PROPERTIES:$IMARKET_API_PROPERTIES -p 80:9090 imarket/imarket-api
