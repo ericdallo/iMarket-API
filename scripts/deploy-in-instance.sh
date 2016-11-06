7909a9020dcb8d401a552014a31d7a391baa3a82
Eric Dallo <ericdallo06@hotmail.com>
2016-11-06 11:51:21 -0200

    Corrige deploy-in-instance.sh

--- scripts/deploy-in-instance.sh
+++ scripts/deploy-in-instance.sh
@@ -2,15 +2,20 @@
 
 BUCKET_DIR=/tmp/bucket
 IMARKET_API_PROPERTIES=/opt/application.properties
+IMARKET_API_GCLOUD_JSON=/opt/gcloud.json
 
 mkdir -p $BUCKET_DIR
 gcsfuse configuration.imarketbr.com $BUCKET_DIR
 
 cp $BUCKET_DIR/imarket-api/prod/production.properties $IMARKET_API_PROPERTIES
+cp $BUCKET_DIR/imarket-api/prod/gcloud.json $IMARKET_API_GCLOUD_JSON
 fusermount -u $BUCKET_DIR
 rm -r $BUCKET_DIR
 
 docker pull imarket/imarket-api:latest
 docker stop imarket-api
 docker rm imarket-api
-docker run --name imarket-api -d -v $IMARKET_API_PROPERTIES:$IMARKET_API_PROPERTIES -p 80:9090 imarket/imarket-api
+docker run --name imarket-api -d \
+			-v $IMARKET_API_PROPERTIES:$IMARKET_API_PROPERTIES \
+			-v $IMARKET_API_GCLOUD_JSON:$IMARKET_API_GCLOUD_JSON \
+			-p 80:9090 imarket/imarket-api
