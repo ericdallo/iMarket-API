9375835e94423a961b63c2722a29f45bf506f3d3
Eric Dallo <ericdallo06@hotmail.com>
2016-11-07 09:44:03 -0200

    Remove gcloud.json do script de deploy

--- scripts/deploy-in-instance.sh
+++ scripts/deploy-in-instance.sh
@@ -2,13 +2,11 @@
 
 BUCKET_DIR=/tmp/bucket
 IMARKET_API_PROPERTIES=/opt/application.properties
-IMARKET_API_GCLOUD_JSON=/opt/gcloud.json
 
 mkdir -p $BUCKET_DIR
 gcsfuse configuration.imarketbr.com $BUCKET_DIR
 
 cp $BUCKET_DIR/imarket-api/prod/production.properties $IMARKET_API_PROPERTIES
-cp $BUCKET_DIR/imarket-api/prod/gcloud.json $IMARKET_API_GCLOUD_JSON
 fusermount -u $BUCKET_DIR
 rm -r $BUCKET_DIR
 
@@ -16,6 +14,5 @@ docker pull imarket/imarket-api:latest
 docker stop imarket-api
 docker rm imarket-api
 docker run --name imarket-api -d \
-			-v $IMARKET_API_PROPERTIES:$IMARKET_API_PROPERTIES \
-			-v $IMARKET_API_GCLOUD_JSON:$IMARKET_API_GCLOUD_JSON \
-			-p 80:9090 imarket/imarket-api
+            -v $IMARKET_API_PROPERTIES:$IMARKET_API_PROPERTIES \            
+            -p 80:9090 imarket/imarket-api
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -28,20 +28,17 @@ docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 
 BUCKET_DIR=/tmp/bucket
 IMARKET_API_PROPERTIES=/opt/application.properties
-IMARKET_API_GCLOUD_JSON=/opt/gcloud.json
 
 mkdir -p $BUCKET_DIR
 gcsfuse configuration.imarketbr.com $BUCKET_DIR
 
 cp $BUCKET_DIR/imarket-api/prod/production.properties $IMARKET_API_PROPERTIES
-cp $BUCKET_DIR/imarket-api/prod/gcloud.json $IMARKET_API_GCLOUD_JSON
 fusermount -u $BUCKET_DIR
 rm -r $BUCKET_DIR
 
 docker pull imarket/imarket-api:latest
-docker stop imarket-api
+docker stop imarket-api # TODO verify if container is running
 docker rm imarket-api
 docker run --name imarket-api -d \
             -v $IMARKET_API_PROPERTIES:$IMARKET_API_PROPERTIES \
-            -v $IMARKET_API_GCLOUD_JSON:$IMARKET_API_GCLOUD_JSON \
             -p 80:9090 imarket/imarket-api
